cf036ced4522fbec9285ea558c98e42482090820
Alina Lavrik <alina_voro@mail.ru>
2022-09-19 22:00:33 +0300

    Add TASK 004

---
+++ TASK04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа 
+
+int number = new Random().Next(10, 100);
+int num1 = number / 10;
+int num2 = number % 10;
+
+int max = num1;
+if (max < num2) 
+{
+    max = num2;
+}
+{
+    Console.WriteLine($"наибольшая цифра числа {number} = {max}");
+}
